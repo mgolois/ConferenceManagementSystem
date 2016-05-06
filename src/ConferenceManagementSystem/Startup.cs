9525ea1cca7e67492a1e0fc1148b0044aa889ab5
mgolois <mgolois@yahoo.com>
2016-05-06 14:26:42 -0400

    initial Checkin

--- src/ConferenceManagementSystem/Startup.cs
+++ src/ConferenceManagementSystem/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CMFIMaryland.Conference
+namespace ConferenceManagementSystem
 {
     public class Startup
     {
